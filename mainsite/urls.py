6b56b3ae3779c98e27b57c97f14a2f796e38727d
phant0mthief1417 <113264956+phant0mthief1417@users.noreply.github.com>
2023-01-09 07:32:06 -0700

    Main page, todo list working
    Main home and todo list project is working, uses the local host database, if another device accesses, they will have to change database to the sqlite3 temporarily. Static images work, need to find a way to add individual project pages to site.

---
+++ mainsite/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from .views import *
+from django.contrib.auth.views import LogoutView
+
+urlpatterns = [
+    path('todo/', TaskList.as_view(), name="task"),
+    path('todo/create/', TaskCreate.as_view(), name="create"),
+    path('todo/login/', CustomLoginView.as_view(), name="login"),
+    path('todo/logout/', LogoutView.as_view(next_page='login'), name="logout"),
+    path('todo/register/', RegisterPage.as_view(), name="register"),
+    path('todo/<int:pk>/', TaskDetail.as_view(), name="detail"),
+    path('todo/update/<int:pk>/', TaskUpdate.as_view(), name="update"),
+    path('todo/delete/<int:pk>/', TaskDelete.as_view(), name="delete"),
+    path('home/', Home_page, name="Home_page"),
+    # path('asteroid/', Asteroid_page, name="Asteroid_page"),
+]
